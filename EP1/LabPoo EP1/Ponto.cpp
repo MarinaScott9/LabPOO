1c60189a40e89686239bdac02a241c5d6c20f432
Marina Scott <31037358+MarinaScott9@users.noreply.github.com>
2017-09-07 17:28:38 -0300

    Pequenas mudanças Ponto.cpp
    atualizei bibliotecas e variavel Coordenadas
    formatei para ficar parecido com o que vimos em aula

--- EP1/LabPoo EP1/Ponto.cpp
+++ EP1/LabPoo EP1/Ponto.cpp
@@ -4,40 +4,40 @@
 // 8994178 Marina Scott do Val
 // 8039011 Pedro Henrique Lage Furtado de Mendonca
 
+#include <iostream>
+using namespace std;
 #include "Ponto.h"
 
-double Ponto::getX() const 
+double Ponto::getX() const
 {
-	return Coords[0];
+	return Coordenada[0];
 }
 
 double Ponto::getY() const
 {
-	return Coords[1];
+	return Coordenada[1];
 }
 
 void Ponto::setX(double x) {
-	Coords[0] = x; return;
+	Coordenada[0] = x; return;
 }
 
 void Ponto::setY(double y) {
-	Coords[1] = y; return;
+	Coordenada[1] = y; return;
 }
 
 void Ponto::imprimir() const {
-	std::cout << "(" << Coords[0] << ", " << Coords[1] << ")\n";
+	cout << "(" << Coordenada[0] << ", " << Coordenada[1] << ")" << endl;
 	return;
 }
 
 bool Ponto::eIgual(const Ponto * outro) const
 {
 	const double Epsilon = 1E-5;
-	double DistX = std::abs(outro->getX() - Coords[0]);
-	double DistY = std::abs(outro->getY() - Coords[1]);
+	double DistX = abs(outro->getX() - Coords[0]);
+	double DistY = abs(outro->getY() - Coords[1]);
 
-	if (DistX <= Epsilon && DistY <= Epsilon) { return true; }
+	if (DistX <= Epsilon && DistY <= Epsilon) return true;
 
 	return false;
 }
-
-
